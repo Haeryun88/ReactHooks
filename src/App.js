f6b99cb6cd6e3a98bfb68d5c309b618621a95945
Haeryun88 <97426995+Haeryun88@users.noreply.github.com>
2022-11-08 16:51:10 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const countRef = useRef(0);
+  //   const [count, setCount] = useState(0);
+  //   const countRef = useRef(0);
 
-  console.log(countRef); //countRef.current
+  //   console.log(countRef); //countRef.current
 
-  const increaseCountState = () => {
-    setCount(count + 1);
+  //   const increaseCountState = () => {
+  //     setCount(count + 1);
+  //   };
+
+  //   const increaseCountRef = () => {
+  //     countRef.current = countRef.current + 1;
+  //   };
+
+  const inputRef = useRef();
+
+  useEffect (()=>{
+    inputRef.current.focus()
+  },[]);
+
+
+  
+  const login = () => {
+  console.log(inputRef.current.value);
+
+    alert(`환영합니다. ${inputRef.current.value}님`);
   };
-
-  const increaseCountRef = () => {
-    countRef.current = countRef.current + 1;
-  };
-
   return (
     <div>
-      <p>State {count}</p>
-      <p>Ref: {countRef.current}</p>
-      <button onClick={increaseCountState}>State 올려</button>
-      <button onClick={increaseCountRef}>Ref 올려</button>
+      <input ref={inputRef} placeholder="name" />
+      <button onClick={login}>Ref 올려</button>
     </div>
   );
 };
