181c1fd739ec75276ec5b1d917b2905bd5de33ec
Haeryun88 <97426995+Haeryun88@users.noreply.github.com>
2022-11-08 16:07:09 +0900

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useRef } from 'react';
 
-export default function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+
+  console.log(countRef); //countRef.current
+
+  const increaseCountState = () => {
+    setCount(count + 1);
+  };
+
+  const increaseCountRef = () => {
+    countRef.current = countRef.current + 1;
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>State {count}</p>
+      <p>Ref: {countRef.current}</p>
+      <button onClick={increaseCountState}>State 올려</button>
+      <button onClick={increaseCountRef}>Ref 올려</button>
     </div>
   );
-}
+};
+
+export default App;
